1c1743f3e5eeee612b2c792f1be0d7cb784beb79
Mate Balogh <b.mate11@gmail.com>
2017-11-08 13:00:28 +0100

    initial commit

---
+++ PingPongGame/Form1.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace PingPongGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.Black;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Location = new System.Drawing.Point(257, 558);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(176, 13);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(278, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(155, 46);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Paused";
+            this.label1.Visible = false;
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.ball.Location = new System.Drawing.Point(145, 139);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(26, 25);
+            this.ball.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.ball.TabIndex = 2;
+            this.ball.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.ClientSize = new System.Drawing.Size(709, 602);
+            this.Controls.Add(this.ball);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "PingPong";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ PingPongGame/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System.Windows.Forms;
+
+namespace PingPongGame
+{
+    public partial class Form1 : Form
+    {
+        int ballTop = 5;
+        int ballLeft = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+            this.TopMost = true;
+        }
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                this.Close();
+            } else if (e.KeyCode == Keys.Space)
+            {
+                if (label1.Visible)
+                {
+                    label1.Hide();
+                    textBox1.Enabled = true;
+                    this.Focus();
+                } else
+                {
+                    label1.Show();
+                    textBox1.Enabled = false;
+                }
+            }
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left)
+            {
+                textBox1.Left -= 10;
+            } else if (e.KeyCode == Keys.Right)
+            {
+                textBox1.Left += 10;
+            }
+        }
+
+        private void timer1_Tick(object sender, System.EventArgs e)
+        {
+            ball.Top += ballTop;
+            ball.Left += ballLeft;
+
+            if (ball.Bottom >= textBox1.Top && ball.Left >= textBox1.Left && ball.Right <= textBox1.Right)
+            {
+                ballTop = -ballTop;
+                ballLeft = -ballLeft;
+            }
+        }
+    }
+}
