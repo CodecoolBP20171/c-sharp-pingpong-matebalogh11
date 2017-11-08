a20377daf5c56f14327ddb32ed7538eb2fafee2a
Mate Balogh <b.mate11@gmail.com>
2017-11-08 19:08:24 +0100

    Game can be paused, ball bounces back from the frame, scoreboard, levels implemented

--- PingPongGame/Form1.Designer.cs
+++ PingPongGame/Form1.Designer.cs
@@ -30,28 +30,34 @@
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.slide = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.ball = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.board = new System.Windows.Forms.Panel();
+            this.level = new System.Windows.Forms.Label();
+            this.scoreBoard = new System.Windows.Forms.Label();
+            this.gameOver = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            this.board.SuspendLayout();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.BackColor = System.Drawing.Color.Black;
-            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.textBox1.Location = new System.Drawing.Point(257, 558);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(176, 13);
-            this.textBox1.TabIndex = 0;
-            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            this.slide.BackColor = System.Drawing.Color.Black;
+            this.slide.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.slide.Location = new System.Drawing.Point(257, 558);
+            this.slide.Name = "textBox1";
+            this.slide.Size = new System.Drawing.Size(176, 13);
+            this.slide.TabIndex = 0;
+            this.slide.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(278, 87);
+            this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label1.Location = new System.Drawing.Point(276, 85);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(155, 46);
             this.label1.TabIndex = 1;
@@ -71,8 +77,54 @@
             // timer1
             // 
             this.timer1.Enabled = true;
-            this.timer1.Interval = 10;
+            this.timer1.Interval = 1;
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.board.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.board.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel1.BackgroundImage")));
+            this.board.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.board.Controls.Add(this.gameOver);
+            this.board.Controls.Add(this.label1);
+            this.board.Controls.Add(this.level);
+            this.board.Controls.Add(this.scoreBoard);
+            this.board.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.board.Location = new System.Drawing.Point(0, 0);
+            this.board.Name = "panel1";
+            this.board.Size = new System.Drawing.Size(709, 602);
+            this.board.TabIndex = 3;
+            // 
+            // level
+            // 
+            this.level.AutoSize = true;
+            this.level.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.level.Location = new System.Drawing.Point(621, 11);
+            this.level.Name = "level";
+            this.level.Size = new System.Drawing.Size(81, 25);
+            this.level.TabIndex = 1;
+            this.level.Text = "Level: 1";
+            // 
+            // scoreBoard
+            // 
+            this.scoreBoard.AutoSize = true;
+            this.scoreBoard.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.scoreBoard.Location = new System.Drawing.Point(11, 11);
+            this.scoreBoard.Name = "scoreBoard";
+            this.scoreBoard.Size = new System.Drawing.Size(86, 25);
+            this.scoreBoard.TabIndex = 0;
+            this.scoreBoard.Text = "Score: 0";
+            // 
+            // gameOver
+            // 
+            this.gameOver.AutoSize = true;
+            this.gameOver.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.gameOver.Location = new System.Drawing.Point(234, 85);
+            this.gameOver.Name = "gameOver";
+            this.gameOver.Size = new System.Drawing.Size(235, 46);
+            this.gameOver.TabIndex = 2;
+            this.gameOver.Text = "Game Over!";
+            this.gameOver.Visible = false;
             // 
             // Form1
             // 
@@ -82,15 +134,17 @@
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(709, 602);
             this.Controls.Add(this.ball);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.slide);
+            this.Controls.Add(this.board);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.KeyPreview = true;
             this.Name = "Form1";
             this.Text = "PingPong";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            this.board.ResumeLayout(false);
+            this.board.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -98,10 +152,14 @@
 
         #endregion
 
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox slide;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox ball;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel board;
+        private System.Windows.Forms.Label scoreBoard;
+        private System.Windows.Forms.Label level;
+        private System.Windows.Forms.Label gameOver;
     }
 }
 
--- PingPongGame/Form1.cs
+++ PingPongGame/Form1.cs
@@ -4,30 +4,37 @@ namespace PingPongGame
 {
     public partial class Form1 : Form
     {
+        bool over = false;
         int ballTop = 5;
         int ballLeft = 5;
+        int score;
+        int gameLevel = 1;
+        bool increased = false;
+        int temp = 0;
+        int slideLeft;
 
         public Form1()
         {
             InitializeComponent();
             this.TopMost = true;
+            slideLeft = slide.Left;
         }
         private void Form1_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.Escape)
             {
                 this.Close();
-            } else if (e.KeyCode == Keys.Space)
+            } else if (e.KeyCode == Keys.Space && !over)
             {
                 if (label1.Visible)
                 {
                     label1.Hide();
-                    textBox1.Enabled = true;
+                    timer1.Enabled = true;
                     this.Focus();
                 } else
                 {
                     label1.Show();
-                    textBox1.Enabled = false;
+                    timer1.Enabled = false;
                 }
             }
         }
@@ -36,23 +43,50 @@ namespace PingPongGame
         {
             if (e.KeyCode == Keys.Left)
             {
-                textBox1.Left -= 10;
+                slideLeft -= 10;
             } else if (e.KeyCode == Keys.Right)
             {
-                textBox1.Left += 10;
+                slideLeft += 10;
             }
         }
 
         private void timer1_Tick(object sender, System.EventArgs e)
         {
+
             ball.Top += ballTop;
             ball.Left += ballLeft;
+            slide.Left = slideLeft;
 
-            if (ball.Bottom >= textBox1.Top && ball.Left >= textBox1.Left && ball.Right <= textBox1.Right)
+            if (ball.Bottom >= slide.Top && ball.Bottom <= slide.Bottom && ball.Left >= slide.Left && ball.Right <= slide.Right)
             {
+                score += 10;
+                scoreBoard.Text = $"Score: {score}"; 
                 ballTop = -ballTop;
+            }
+            if (ball.Left <= board.Left || ball.Right >= board.Right)
+            {
                 ballLeft = -ballLeft;
             }
+            if (ball.Top <= board.Top)
+            {
+                ballTop = -ballTop;
+            }
+            if (ball.Bottom >= board.Bottom)
+            {
+                slide.Enabled = false;
+                ball.Hide();
+                gameOver.Show();
+                over = true;
+            }
+            if (score % 30 == 0 && !increased && score > 0)
+            {
+                increased = true;
+                ballLeft = (ballLeft > 0) ? ballLeft + 5 : ballLeft - 5;
+                ballTop = (ballTop > 0) ? ballTop + 5 : ballTop - 5;
+                temp = score;
+                level.Text = $"Level: {++gameLevel}";
+            }
+            if (temp != score && increased) increased = false;
         }
     }
 }
